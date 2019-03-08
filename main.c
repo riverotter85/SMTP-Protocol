c5736ce297f636fe3a15f3d5f232af69c010c9df
Logan Davis <overhome2@gmail.com>
2019-03-07 18:43:17 -0600

    Wrote driver that writes message by user and reads response from server. Code has not yet been tested. Added gitignore file.

--- main.c
+++ main.c
@@ -1,24 +1,101 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-const char* get_ip_from_domain(const char* domain_name) {
-    const char* domain_ip;
-    struct in_addr
-}
+//const char* get_ip_from_domain(const char* domain_name) {
+//    const char* domain_ip;
+//    struct in_addr
+//}
 
-int connect_to_server(const char* server_name) {
-    int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
-    struct sockaddr_in addr;
+//int connect_to_server(const char* server_name) {
+//    int socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+//    struct sockaddr_in addr;
+//
+//    memset(&addr, 0, sizeof(addr));
+//    addr.sin_family = AF_INET;
+//    addr.sin_port = htons(25); // SMTP port
+//
+//    if ()
+//}
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(25); // SMTP port
+int main()
+{
+    int socket_fd;
+    struct sockaddr_in host_addr;
+    struct hostent *host_info;
 
-    if ()
-}
+    socklen_t host_addr_size;
 
-int main() {
+    char buffer[256];
 
+    // Create socket; returns 0 for success, -1 for failure
+    socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    if (!socket_fd)
+    {
+        perror("Failed to create socket.");
+        return 1;
+    }
+
+    memset(&host_addr, 0, sizeof(struct sockaddr_in)); // Clear my_addr struct
+    host_addr.sin_family = AF_INET;
+    host_addr.sin_port = htons(25); // SMTP port
+    host_info = gethostbyname("*** Host Name ***");
+    if (!host_info)
+    {
+        perror("Failed to obtain host info.");
+        return 1;
+    }
+    host_addr.sin_addr = *(struct in_addr *) host_info->h_addr; // NOTE: May need to change!
+
+    // Connect socket to host
+    if (!connect(socket_fd, &host_addr, sizeof(host_addr)))
+    {
+        perror("Socket failed to connect to host.");
+        return 1;
+    }
+
+    printf("Enter a message to send: ");
+    memset(&buffer, 0, 256) // Keep an eye on this e_e
+    fgets(buffer, 255, stdin);
+    if (!write(socket_fd, buffer, strlen(buffer)))
+    {
+        perror("Error writing to socket.");
+        return 1;
+    }
+    printf("WRITE: %s\n", buffer);
+
+    memset(&buffer, 0, 256);
+    if (!read(socket_fd, buffer, 255))
+    {
+        perror("Error reading from socket.");
+        return 1;
+    }
+
+    //// Bind address to socket
+    //if (!bind(socket_fd, (struct sockaddr *) &my_addr, sizeof(my_addr)))
+    //{
+    //    perror("Failed to bind socket to address.");
+    //    return 1;
+    //}
+
+    //// Listen for request from server
+    //if (!listen(socket_fd, LISTEN_BACKLOG))
+    //{
+    //    perror("Socket failed to listen to host.");
+    //    return 1;
+    //}
+
+    //// Accept
+    //host_addr_size = sizeof(struct sockaddr_in);
+    //if (!accept(socket_fd, (struct sockaddr *) &host_addr, &host_addr_size))
+    //{
+    //    perror("Failed to accept response from host.");
+    //    return 1;
+    //}
+
+    // Write reply to socket
+
+    // Close socket
 }
